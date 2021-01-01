ba98a1248be3d38f74ff52e6714088c98fc31a9d
Isabelle Villasenor <villasenori@outlook.com>
2021-01-01 15:59:37 -0700

    Add Validations to Bachelorette Model and Model Spec

--- spec/models/bachelorette_spec.rb
+++ spec/models/bachelorette_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Bachelorette, type: :model do
+describe Bachelorette do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:season_description) }
+    it { should validate_numericality_of(:season_number).is_greater_than(0) }
+  end
+
   describe 'relationships' do
     it {should have_many :contestants}
   end
